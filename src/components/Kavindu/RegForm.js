352ff022bcd194410dfef3b26a6070a2dc91ae11
Kavinduweb <kavindupro40@gmail.com>
2021-09-07 23:50:40 +0530

    repo gen

--- src/components/Kavindu/RegForm.js
+++ src/components/Kavindu/RegForm.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom';
-import './Form.css';
+import '../../../src/Form.css';
 import axios from "axios";
 import { configure } from '@testing-library/react';
 
