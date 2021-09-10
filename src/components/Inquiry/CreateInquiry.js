5392699f0fbeee8a829739b0dbd1a2cce27845df
VihangaJanith <it20219420@my.sliit.lk>
2021-09-10 12:11:52 +0530

    Inquiry1

---
+++ src/components/Inquiry/CreateInquiry.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import './inq.css'
+import four from "./four.jpg";
+
+
+
+
+
+export default class CreateInquiry extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name:"",
+            nic:"",
+            phone:"",
+            email:"",
+            inq:"",
+            formErrors : {name:'', nic:'',phone:'',email:'',inq:''},
+            nameValid:false,
+            nicValid:false,
+            phoneValid:false,
+            emailValid:false,
+            inqValid:false
+        }
+
+    }
+    handleInputChange = (e) =>{
+        const {name,value} = e.target;
+        this.setState({
+            ...this.state,
+            [name]:value
+            
+        })
+    }
+    
+    onSubmit= (e) =>{
+        e.preventDefault();
+
+        const {name,nic,phone,email,inq} = this.state;
+
+        const data= {
+            name:name,
+            nic:nic,
+            phone:phone,
+            email:email,
+            inq:inq            
+        }
+
+        console.log(data)
+
+        axios.post("http://localhost:8070/inquiry/add",data).then((res) =>{
+           
+        if(res.data.success){
+            alert("You Have Successfully Created an Inquiry")
+            
+                this.setState({
+                    name:"",
+                    nic:"",
+                    phone:"",
+                    email:"",
+                    inq:""
+                })
+
+            }
+            
+
+        })
+
+        const name1 = document.getElementById('name');
+        const nic1 = document.getElementById('nic');
+        const email1 = document.getElementById('email');
+        const phone1 = document.getElementById('phone');
+        const inq1 = document.getElementById('inq');
+            if (name1.value === '' || name1.value == null){
+                alert('Please enter Your Name')
+                return false;
+        }
+            else if (nic1.value=== ''||nic1.value == null){
+                alert('Please enter Your NIC')
+                return false;
+        }
+            else if (email1.value === '' || email1.value == null){
+                alert('Please enter Your Email Address')
+                return false;
+        }
+            else if (phone1.value === '' || phone1.value == null){
+                alert('Please enter Your Phone Number')
+                return false;
+        } 
+            else if (inq1.value === '' || inq1.value == null){
+                alert('Please enter Your Inquiry')
+                return false;
+        }
+ 
+    }
+    
+
+     
+    
+
+
+    render(){
+        return(
+            <div className="vj" >
+            <div>
+            
+            <div className="needs-validation">
+          
+                <form  noValidate  >
+                <h2 > Create New Inquiry</h2>
+                <hr/>
+
+                    <div className="" style={{marginBottom:'15px'}} >
+                        <label style={{marginBottom:'5px'}}>Name</label>
+                            <input 
+                            type="text"
+                            id="name"
+                            className="form-control"                
+                            name="name"
+                            placeholder="Enter Your Name"
+                            value={this.state.name} 
+                            onChange={this.handleInputChange}/>
+
+                    </div>
+            
+                    <div className="" style={{marginBottom:'15px'}} >
+                        <label style={{marginBottom:'5px'}}>NIC</label>
+                            <input 
+                            type="text" 
+                            id="nic"
+                            className="form-control"                
+                            name="nic"
+                            placeholder="Enter Your NIC"
+                            value={this.state.nic} 
+                            onChange={this.handleInputChange}/>
+                           
+                    </div>
+
+                    <div className="form-group" style={{marginBottom:'15px'}}>
+                        <label style={{marginBottom:'5px'}}>Phone Number</label>
+                            <input 
+                            type="text" 
+                            id="phone"
+                            className="form-control"                
+                            name="phone"
+                            placeholder="Enter Your Phone Number"
+                            value={this.state.phone} 
+                            onChange={this.handleInputChange}/>
+                           
+                    </div>
+
+                    <div className="form-group" style={{marginBottom:'15px'}}>
+                        <label style={{marginBottom:'5px'}}>Email</label>
+                            <input 
+                            type="email" 
+                            className="form-control"
+                            id="email"             
+                            name="email"
+                            placeholder="Enter Your Email"
+                            value={this.state.email} 
+                            onChange={this.handleInputChange}/>
+                         
+                    </div>
+
+                    <div className="form-group" style={{marginBottom:'15px'}}>
+                        <label style={{marginBottom:'5px'}}>Inquiry</label>
+                            <input 
+                            type="text"
+                            id="inq" 
+                            className="form-control"                
+                            name="inq"
+                            placeholder="Enter Your Inquiry"
+                            value={this.state.inq} 
+                            onChange={this.handleInputChange}/>
+                          
+                    </div>
+
+                    <button className="btn btn-dark" type="submit" style={{margintop:'15px'}} onClick={this.onSubmit}>
+                        <i className="fa fa-check-square"></i>    
+                        &nbsp; Create Inquiry    
+                    </button>
+
+                        &nbsp;
+                 
+                    <a href={"/view"} > <button className="btn btn-dark"type="button" style={{margintop:'15px'}} > 
+                        <i className="fa fa-chevron-circle-right"></i>    
+                        &nbsp; Previous Inquiries
+                        </button>
+                    </a>  
+
+                </form>
+      
+            </div>
+        </div> 
+        </div>   
+        )
+
+    }
+}
---
+++ src/components/Inquiry/InqView.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+
+
+export default function InqView (){
+   
+  
+	const [nic,setNic] = useState("");
+   
+
+    const getData =async (e)=>{
+
+        e.preventDefault();
+
+        const nic1 = nic;
+        const url="http://localhost:8070/inquiry/view/";
+
+     
+        const res = await axios.get(url+nic1).then((res)=> {
+
+        document.getElementById("name").innerHTML =res.data.name;
+        document.getElementById("email").innerHTML =res.data.email;
+        document.getElementById("phone").innerHTML =res.data.phone;
+        document.getElementById("inq").innerHTML =res.data.inq;
+        const mongoid=res.data.id;
+          
+       
+           if(res=true) {
+                alert("You are a valid user");
+                window.location.replace("/inqD/" + mongoid)
+
+           }
+           if(res=false) {
+                window.location.replace("/add")  
+           }
+           
+
+            }).catch((err)=>{
+            alert('You Are Not a Verified User!!');
+    })
+
+    }
+
+
+
+    
+    
+   
+    
+    return(
+        <div >
+
+                <div >
+		            <form className="needs-validation-view"   >
+			
+                    <h3>Enter Your National Identity Card Number (NIC)</h3>
+                        <hr/>
+			            <input type="nic" id ="nic" placeholder="NIC" value={nic} onChange={(e)=>{  setNic(e.target.value) ; }}/>
+                        <br/>
+                        <a>
+			                <button  className="btn mt-3 btn-dark"  style={{textDecoration:'none',color:'white'}} onClick={getData}>
+                            <i className="fa fa-arrow-circle-right"></i> Submit
+                            </button>
+                        </a>
+                    </form>
+
+                    < div style={{visibility: 'hidden'}} >
+                        <h1 id="name" ></h1>
+                        <h2 id="nic" ></h2>
+                        <h2 id="email" ></h2>
+                        <h2 id="phone" ></h2>
+                        <h2 id="inq" ></h2>
+                    </div>
+	            </div>
+            </div>
+         
+
+            )
+    }
