7e88da9ba6cfb55e8bce12e544e100c184c7347a
Kamindu99 <kamidugayantha123@gmail.com>
2021-09-15 22:14:25 +0530

    header footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,8 @@ import "../Styles/HeaderFooter.css";
 function Footer() {
   return (
     <div className="mainfooter">
-      
-       <div className="ftr">
-	    <div className="row">
+      <div className="ftr">
+        <div className="row">
           {/* Column1 */}
           <div className="col">
             <h4>Visit Us</h4>
@@ -14,56 +13,62 @@ function Footer() {
               <li>26/D</li>
               <li>Malabe</li>
               <li>Kaduwela</li>
+              <li>Colombo</li>
             </h6>
           </div>
           {/* Column2 */}
           <div className="col">
             <h4>Terms</h4>
-			<h6 className="list-unstyled">
-        
-              <li>xxxxxxx</li>
-              <li>xxxxxxx</li>
-              <li>xxxxxxx</li>
-            
-			</h6>
+            <h6 className="list-unstyled">
+              <li>Pay visa</li>
+              <li>Copy Right</li>
+              <li>Must Agree</li>
+              <li> Must Read</li>
+            </h6>
           </div>
           {/* Column3 */}
           <div className="col">
             <h4>Services</h4>
             <h6 className="list-unstyled">
-              <li>xxxxx</li>
-              <li>xxxxx</li>
-              <li>xxxxxx</li>
+              <li>Trip Packages </li>
+              <li>Rooms Packages</li>
+              <li>Activity</li>
+              <li>Equipments</li>
             </h6>
           </div>
         </div>
-        
-        </div>
-        
-        <hr  />
-		<div className="down">
+      </div>
+
+      <hr />
+      <div className="down">
         <div className="row">
           <p className="col-sm">
-			  
-
-        	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-       
-			<a href="#"><i class="fab fa-facebook-f" style={{color:"#adadad"}}></i></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="#"><i class="fab fa-twitter" style={{color:"#adadad"}}></i></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="#"><i class="fab fa-instagram" style={{color:"#adadad"}}></i></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <br/><br/>
-            &copy;{new Date().getFullYear()} Dream Travelers | All rights reserved |
-            Terms Of Service | Privacy
-            
-		
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#">
+              <i class="fab fa-facebook-f" style={{ color: "#adadad" }}></i>
+            </a>{" "}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#">
+              <i class="fab fa-twitter" style={{ color: "#adadad" }}></i>
+            </a>{" "}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="#">
+              <i class="fab fa-instagram" style={{ color: "#adadad" }}></i>
+            </a>{" "}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            &copy;{new Date().getFullYear()} Dream Travelers | All rights
+            reserved | Terms Of Service | Privacy
           </p>
         </div>
-		</div>
-		
       </div>
-    
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,66 +1,152 @@
 import React from "react";
-import '../Styles/HeaderFooter.css'
+import "../Styles/HeaderFooter.css";
 
-function Header(){
-
-    return(
-
-<div class="wrapper">
-    <div class="sidebar">
-       <h1>  <i class="fas fa-plane-departure"> </i></h1>  
+function Header() {
+  return (
+    <div class="wrapper">
+      <div class="sidebar">
+       
         <ul>
-            <li><a href="/"  style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-home"></i>Home</a></li>
-            <li><a href="/travelpackages" style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-car"></i>Travel Package</a></li>
-            <li><a href="/hotelpackage" style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-bed"></i>Room Package</a></li>
-            <li><a href="/all" style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-running"></i>Activity</a></li>
-            <li><a href="/equipment" style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-campground"></i>Equipment</a></li>
-            <li><a href="/guide/all"  style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-home"></i>Guides</a></li>
-            <li><a href="/feedback" style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-comments"></i>FeedBack</a></li>
-            <li><a href="/add"style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-exclamation-circle"></i>Inquiry</a></li>
-            <li><a href="/adminhome"style={{textDecoration:'none' ,color:'white'}}><i class="fas fa-exclamation-circle"></i>Admin</a></li>
-        </ul> 
+        <li> 
+           
+      
+         
+         
+               
+               
+              
+                {" "}
+                <i class="fas fa-plane-departure"  style={{ textDecoration: "none", color: "rgba(116, 116, 116, 0)",fontSize:"105px"}}> </i>
+            
+        
+            <a href="/" style={{ textDecoration: "none", color: "white" }}>
+              <i class="fas fa-home"></i>Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/travelpackages"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <i class="fas fa-car"></i>Travel Package
+            </a>
+          </li>
+          <li>
+            <a
+              href="/hotelpackage"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <i class="fas fa-bed"></i>Room Package
+            </a>
+          </li>
+          <li>
+            <a href="/all" style={{ textDecoration: "none", color: "white" }}>
+              <i class="fas fa-running"></i>Activity
+            </a>
+          </li>
+          <li>
+            <a
+              href="/equipment"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <i class="fas fa-campground"></i>Equipment
+            </a>
+          </li>
+          <li>
+            <a
+              href="/guide/all"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <i class="fas fa-home"></i>Guides
+            </a>
+          </li>
+          <li>
+            <a
+              href="/feedback"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <i class="fas fa-comments"></i>FeedBack
+            </a>
+          </li>
+          <li>
+            <a href="/add" style={{ textDecoration: "none", color: "white" }}>
+              <i class="fas fa-exclamation-circle"></i>Inquiry
+            </a>
+          </li>
+         
+        </ul>
         <div class="social_media">
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+
+      <div class="main_content">
+        <div class="header">
+        
+     
+          <div>
+            <div id="hname">
+              <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b> Dream Travelers (pvt)</b> </h1>
+            </div>
+
+        <div id="logott">
+              <a
+                href="/"
+                style={{ textDecoration: "none", color: "rgb(218, 213, 213)"}}
+              >
+                {" "}
+                <i class="fas fa-plane-departure"> </i>
+              </a>
+            </div>
+
+            <div id="login">
+              <a
+                href="/register"
+                style={{ textDecoration: "none", color: "rgb(218, 213, 213)"}}
+              >
+                {" "}
+                <i class="fas fa-user-alt"></i>{" "}
+              </a>
+            </div>
+          </div>
+
+          <div className="menupp" style={{ width: "100%" ,backgroundColor:"hsla(101, 27%, 100%, 0.1)" }}>
+            <li className="menupp">
+              <a></a>
+            </li>
+           
+          
+            <li className="menupp mr-0">
+              <a></a>
+            </li>
+ 
+            <li class="menupp">
+              <a class="actively" href="/aboutus">
+                About Us
+              </a>
+            </li>
+            <li class="menupp">
+              <a href="/services">Services</a>
+            </li>
+            <li class="menupp">
+              <a href="/contactus">Contact Us</a>
+            </li>
+            <li class="menupp">
+              <a href="/gallery">Gallery</a>{" "}
+            </li>
+          </div>
+        </div>
       </div>
     </div>
-    
-    <div class="main_content" >
-        <div class="header">
-    
-        <div class ="header">
-
-<div id="hname">
-    
-  <h1> Dream Travelers (pvt) </h1>
- </div> 
-
-
-<div id="login">
-      <a href="/register"  style={{textDecoration:'none' ,color:'hsl(0,0%,70%,0.9)'}}  > <i class="fas fa-user-alt"></i> </a>
-</div>
-
-</div>
-
-<div className="menupp" style={{width:"100%"}}>
-    <li className="menupp"><a></a></li>
-    <li className="menupp"><a></a></li>
-    
-  <li class="menupp"><a class="actively" href="/aboutus">About Us</a></li>
-  <li class="menupp"><a href="/services">Services</a></li>
-  <li class="menupp"><a href="/contactus">Contact Us</a></li>
-  <li class="menupp"><a href="/gallery">Gallery</a> </li>
-
-  </div>
-
-  </div>  
-    </div>
-</div>
-
-    )
-
-    
+  );
 }
 
 export default Header;
