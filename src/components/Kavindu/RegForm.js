6a1d168cc8de6cc8561070c3cb774ee37281b638
Kavindu perera <59799261+Kavinduweb@users.noreply.github.com>
2021-10-07 12:56:17 +0530

    Update RegForm.js

--- src/components/Kavindu/RegForm.js
+++ src/components/Kavindu/RegForm.js
@@ -115,8 +115,8 @@ return (
 	<div className="form-container form-containerabc sign-up-container sign-up-containerabc">
 	  
 		<form onSubmit={sendData} className="form12">
-			<h1 className="h111">Create Account</h1>
-		
+			<h1 className="h111">Create Account close since 10/07/2021</h1>
+			
 			<input className ="inputabc" type="text" placeholder="Name" id="Name" onChange={(e)=>{  setName(e.target.value) ; }} required />
 			<input  className ="inputabc" type="email" placeholder="Email" id="Email" onChange={(e)=>{  setEmail(e.target.value) ; }}  required/>
         	 <input  className ="inputabc" type="number" placeholder="Mobile" id="Number" onChange={(e)=>{ setNum (e.target.value) ; }} required/>
